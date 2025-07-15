2f2124a489bf287409647a583928a1b8e4364c27
Amanarun2907 <aman29.10.2004@gmail.com>
2025-07-15 11:55:03 +0530

    this is my commit

--- basic_commands.py
+++ basic_commands.py
@@ -1,1 +1,14 @@
 ## git --version (version of my git in cmd)
+## git branch
+## git remote -v
+## git status
+## git add .
+## git commit -m "message"
+## git push origin main
+## git pull origin main
+## git clone https://github.com/23-Git-And-Github/basic_commands.git
+## git push origin main
+## git init (creates a new repository)
+## git config --global user.name "username"
+## git config --global user.email "email"
+## git remote add origin "repo link"
